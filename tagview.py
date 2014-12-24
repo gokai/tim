0ec2d303abb547145e90710f7ce61df93f2dce3a
tachara <tortured.flame@gmail.com>
2014-12-24 12:34:52 +0200

    Added creation of the view to tagview.

--- tagview.py
+++ tagview.py
@@ -1,4 +1,24 @@
+from tkinter.ttk import Treeview
+
+import keybindings as kb
 
 class TagView(object):
-    def __init__():
+
+    def __init__(self, main, tags):
+        self.tags = sorted(tags)
+        self.main = main
+        self.view = Treeview(main.root, columns=["name"])
+        actions = {'search': lambda e: self.search(),
+                   'edit': lambda e: self.edit()}
+        #TODO: keybindings
+        #kb.make_bindings(kb.tagview, actions, self.view.bind)
+        self.view['show'] = 'tree'
+        for tag in self.tags:
+            self.view.insert('', 'end', iid=tag, text=tag)
+
+    def search(self):
         pass
+
+    def edit(self):
+        pass
+
