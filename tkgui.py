e64d79c6beecc5d290c04f3243f90c8f2990373a
tachara <tortured.flame@gmail.com>
2014-12-24 13:54:55 +0200

    Changed Notebook to PanedWindow in class Main.
    Also made the first view to be always visible on the leftmost pane.

--- tkgui.py
+++ tkgui.py
@@ -9,6 +9,7 @@ from tkinter.ttk import *
 from db import FileDatabase
 from tkgraphics import SlideShow, Gallery
 from tkeditview import EditView
+from tagview import TagView
 import keybindings as kb
 
 class FileView(Treeview):
@@ -114,9 +115,8 @@ class Main(object):
 
     def __init__(self):
         root = Tk()
-        self.notebook = Notebook(root)
-        self.notebook.enable_traversal()
-        self.root = self.notebook
+        self.paned_win = PanedWindow(root, orient=HORIZONTAL)
+        self.root = self.paned_win
         root.title('GICM')
         root.focus_set()
         root.rowconfigure(0, weight=1)
@@ -130,32 +130,38 @@ class Main(object):
         kb.make_bindings(kb.appwide,{'quit':lambda e: self.quit(),
             'next_view':lambda e: self.next_view(), 'delete_view': self.delete_current_view},
             self.root.bind_all)
-        self.notebook.grid(row=0, column=0, sticky=(N, S, W, E))
+        self.paned_win.grid(row=0, column=0, sticky=(N, S, W, E))
         self._root = root
 
     def new_view(self, view):
         self.views.append(view)
+        if len(self.views) > 2:
+            self.paned_win.forget(self.views[self.cur_view])
         self.cur_view = len(self.views) - 1
-        self.notebook.add(view, text='Tab {}'.format(self.cur_view))
-        self.notebook.select(view)
+        if len(self.views) == 1:
+            self.paned_win.add(view, weight=1)
+        else:
+            self.paned_win.add(view, weight=5)
+
         view.focus_set()
 
     def remove_view(self, view):
         self.views.remove(view)
-        self.notebook.forget(view)
-        if len(self.views) >= 1:
+        self.paned_win.forget(view)
+        if len(self.views) >= 2:
             self.views[-1].focus_set()
             self.cur_view -= 1
-            self.notebook.select(self.cur_view)
+            self.paned_win.add(self.views[self.cur_view], weight=5)
         else:
             self.quit()
 
     def next_view(self):
+        self.paned_win.forget(self.views[self.cur_view])
         self.cur_view += 1
         if self.cur_view >= len(self.views):
-            self.cur_view = 0
+            self.cur_view = 1
         new_view = self.views[self.cur_view]
-        self.notebook.select(new_view)
+        self.paned_win.add(new_view, weight=5)
         new_view.focus_set()
 
     def display(self):
@@ -166,8 +172,11 @@ class Main(object):
 
 if __name__ == "__main__":
     db = FileDatabase('master.sqlite')
-    li = db.search_by_tags(['nsfw'])
+    li = db.search_by_tags(['abstract'])[:100]
+    tags = db.list_tags()
     gui = Main()
+    view = TagView(gui, tags)
+    gui.new_view(view.view)
     gui.new_view(FileView(gui, li, ('name', 'tags')))
     gui.display()
 
