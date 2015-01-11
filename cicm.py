5930a65ffdf83bb1fa44bb2d75d732591976e523
tachara <tortured.flame@gmail.com>
2015-01-11 18:39:15 +0200

    Fixed tag editing showing wrong value.

--- cicm.py
+++ cicm.py
@@ -54,7 +54,8 @@ if __name__ == "__main__":
     # Custom virtual events do not need to be user bindable
     # since their generation is user bindable. -> no dictionaries used.
     mainview.root.bind_all('<<TagViewSearch>>', glue.search)
-    mainview.root.bind_all('<<TagViewEdit>>', lambda e: mainview.text_query('Edit tag:', e.widget.selection()[0]))
+    mainview.root.bind_all('<<TagViewEdit>>',
+            lambda e: mainview.text_query('Edit tag:', tview.selection()[0]))
     mainview.root.bind_all('<<MainQueryAccept>>', glue.add_or_rename_tags)
 
     mainview.add_sidebar(tview)
