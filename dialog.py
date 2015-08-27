5687f7e4b538dc0e25cc8a21eab848a1f47af0e3
tachara <tortured.flame@gmail.com>
2015-08-27 18:01:31 +0300

    Change ListDialog to show long messages better.
    Uses canvas and scrollbars instead of a label.

--- dialog.py
+++ dialog.py
@@ -1,6 +1,6 @@
 
-from tkinter import Toplevel, Listbox, StringVar, N, S, W, E, EXTENDED
-from tkinter.ttk import Label, Button
+from tkinter import Toplevel, Listbox, Canvas, StringVar, N, S, W, E, EXTENDED, NW, HORIZONTAL, SCROLL, UNITS
+from tkinter.ttk import Label, Button, Scrollbar, Frame
 
 from tagview import NameView
 
@@ -17,8 +17,28 @@ class ListDialog(object):
         self.top.columnconfigure(1, weight=1)
         self.top.resizable(width=True, height=True)
 
-        self.label = Label(self.top, text=message)
-        self.label.grid(row=0, column=0, columnspan=2, sticky=(N, W))
+        self.frame = Frame(self.top)
+        self.frame.rowconfigure(0, weight=1)
+        self.frame.rowconfigure(1, weight=0)
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.columnconfigure(1, weight=0)
+        self.frame.grid(row=0, column=0, sticky=(N, S, W, E), columnspan=2)
+        self.canvas = Canvas(self.frame)
+        self.canvas.create_text(0, 0, text=message, anchor=NW)
+        self.canvas.grid(row=0, column=0, sticky=(N, W, S, E))
+
+        self.vscroll = Scrollbar(self.frame, command=self.canvas.yview)
+        self.vscroll.grid(row=0, column=1, sticky=(N, S))
+        self.canvas['yscrollcommand'] = self.vscroll.set
+
+        self.hscroll = Scrollbar(self.frame, command=self.canvas.xview, orient=HORIZONTAL)
+        self.hscroll.grid(row=1, column=0, sticky=(W, E), columnspan=2)
+        self.canvas['xscrollcommand'] = self.hscroll.set
+
+        self.canvas['scrollregion'] = self.canvas.bbox('all')
+        self.canvas.bind('<Button-4>', self.scroll)
+        self.canvas.bind('<Button-5>', self.scroll)
+        self.canvas.bind('<MouseWheel>', self.scroll)
 
         self.view = NameView(self.top, sorted(items))
         self.view.widget.grid(row=1, column=0, columnspan=2, sticky=(N, W, E, S))
@@ -27,6 +47,7 @@ class ListDialog(object):
         self.cancelbutton = Button(self.top, text='Cancel', command=self.cancel)
         self.delbutton.grid(row=2, column=0)
         self.cancelbutton.grid(row=2, column=1)
+        self.view.widget.focus_set()
 
     def accept(self):
         self.accept_func(self.view.selection())
@@ -35,3 +56,9 @@ class ListDialog(object):
     def cancel(self):
         self.result = None
         self.top.destroy()
+
+    def scroll(self, event):
+        if event.num == 4 or event.delta > 0:
+            self.canvas.yview(SCROLL, -1, UNITS)
+        elif event.num == 5 or event.delta < 0:
+            self.canvas.yview(SCROLL, 1, UNITS)
