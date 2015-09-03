f8e0ce64eda1f7983352e077253ba22b06c019da
gokai <tortured.flame@gmail.com>
2015-09-03 16:25:24 +0300

    Make possible to edit tags from selection tags view

--- tim.py
+++ tim.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     # since their generation is user bindable. -> no dictionaries used.
     mainview.root.bind_all('<<TagViewSearch>>', glue.search_event)
     mainview.root.bind_all('<<TagViewEdit>>',
-            lambda e: mainview.text_query('Edit tag:', tview.selection()[0]))
+            lambda e: mainview.text_query('Edit tag:', e.widget.view.selection()[0]))
     mainview.root.bind_all('<<MainQueryAccept>>', glue.add_or_rename_tags)
     mainview.root.bind_all('<<GallerySelectionChanged>>', glue.update_selection_tags)
     mainview.root.bind_all('<<SlideShowNext>>', glue.update_selection_tags)
